4a9c909e081ebba0d5718ad8123f7b2b4ac3d3d7
dhermiboats <120996278+gourcetools@users.noreply.github.com>
2023-04-14 11:52:18 +0200

    Better logic for screenshot-combiner

--- src/screenshot-combiner.py
+++ src/screenshot-combiner.py
@@ -1,57 +1,65 @@
-from PIL import Image, ImageDraw, ImageOps, ImageFilter
-from PIL.ImageFilter import Kernel
-from PIL.ImageChops import add
-from PIL.ImageFilter import GaussianBlur
-from PIL.Image import Resampling
+import cv2
+import numpy as np
+from PIL import Image, ImageDraw, ImageOps
 
-def add_shadow(image, offset=(5, 5), background_color=(0, 0, 0, 0), shadow_color=(0, 0, 0), blur_radius=15):
-    shadow = Image.new("RGBA", image.size, background_color)
-    shadow.paste(shadow_color, (offset[0], offset[1], image.width + offset[0], image.height + offset[1]))
-    shadow = shadow.filter(ImageFilter.GaussianBlur(blur_radius))
-    result = Image.alpha_composite(shadow, image.convert("RGBA"))
-    return result.convert(image.mode)
+def round_corners(image, radius):
+    circle = Image.new('L', (radius * 2, radius * 2), 0)
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, radius * 2, radius * 2), fill=255)
 
-def rounded_rectangle(size, radius, fill):
-    width, height = size
-    rectangle = Image.new("L", size, 0)
-    draw = ImageDraw.Draw(rectangle)
-    draw.ellipse((0, 0, 2 * radius, 2 * radius), fill=fill)
-    draw.rectangle((radius, 0, width - radius, height), fill=fill)
-    draw.rectangle((0, radius, width, height - radius), fill=fill)
-    draw.ellipse((width - 2 * radius, height - 2 * radius, width, height), fill=fill)
-    return rectangle
+    alpha = Image.new('L', image.size, 255)
+    w, h = image.size
 
-def overlay_images(far_img_path, close_img_path, output_img_path, position=(0, 0), box_size=(300, 150), border_color=(0, 0, 0), border_width=2, line_color=(0, 0, 0)):
-    far_image = Image.open(far_img_path)
-    close_image = Image.open(close_img_path)
+    alpha.paste(circle.crop((0, 0, radius, radius)), (0, 0))
+    alpha.paste(circle.crop((0, radius, radius, radius * 2)), (0, h - radius))
+    alpha.paste(circle.crop((radius, 0, radius * 2, radius)), (w - radius, 0))
+    alpha.paste(circle.crop((radius, radius, radius * 2, radius * 2)), (w - radius, h - radius))
 
-    # Resize the close-up image to fit inside the box
-    close_image_resized = close_image.resize(box_size, Resampling.LANCZOS)
+    image.putalpha(alpha)
+    return image
 
-    # Add a border to the close-up image
-    border_radius = 10
-    close_image_with_border = ImageOps.expand(close_image_resized, border=border_width, fill=border_color)
+def add_border(image, border_size, border_color):
+    return ImageOps.expand(image, border=border_size, fill=border_color)
 
-    # Create the alpha mask with rounded corners for the close-up image with the border
-    mask = rounded_rectangle(close_image_with_border.size, border_radius, 255)
-    close_image_with_border.putalpha(mask)
+def find_best_location(zoomed_out, zoomed_in):
+    zoomed_out_gray = cv2.cvtColor(np.array(zoomed_out), cv2.COLOR_RGB2GRAY)
+    zoomed_in_gray = cv2.cvtColor(np.array(zoomed_in), cv2.COLOR_RGB2GRAY)
 
-    # Add a shadow to the close-up image
-    close_image_with_shadow = add_shadow(close_image_with_border)
+    result = cv2.matchTemplate(zoomed_out_gray, zoomed_in_gray, cv2.TM_CCOEFF_NORMED)
+    _, _, min_loc, max_loc = cv2.minMaxLoc(result)
 
-    # Calculate the position for the close-up image
-    far_width, far_height = far_image.size
-    position = (far_width - box_size[0] - border_width - 10, 10)
+    # Return the location with the highest similarity
+    return max_loc
 
-    # Overlay the close-up image on the far image
-    far_image.paste(close_image_with_shadow, position, close_image_with_shadow)
+def combine_screenshots(zoomed_out_path, zoomed_in_path, output_path):
+    # Load images
+    zoomed_out = Image.open(zoomed_out_path)
+    zoomed_in = Image.open(zoomed_in_path)
 
-    far_image.save(output_img_path)
+    # Resize the zoomed-in image
+    zoomed_in = zoomed_in.resize((zoomed_out.width // 3, zoomed_out.height // 3))
 
-if __name__ == "__main__":
-    far_image_path = "../data/far.png"
-    close_image_path = "../data/close.png"
-    output_image_path = "../data/plane.png"
-    box_size = (300, 150) # Adjust the box size as needed
+    # Round the corners and add a black border to the zoomed-in image
+    zoomed_in = round_corners(zoomed_in, 20)
+    zoomed_in = add_border(zoomed_in, 2, 'black')
 
-    overlay_images(far_image_path, close_image_path, output_image_path, box_size=box_size)
+    # Add a black border to the zoomed-out image
+    zoomed_out = add_border(zoomed_out, 2, 'black')
+
+    # Find the best location to place the zoomed-in image
+    best_location = find_best_location(zoomed_out, zoomed_in)
+
+    # Create a new image with a white background to combine the images
+    combined = Image.new('RGB', (zoomed_out.width, zoomed_out.height), color='white')
+    combined.paste(zoomed_out, (0, 0))
+    combined.paste(zoomed_in, best_location, zoomed_in)
+
+    # Save the combined image
+    combined.save(output_path)
+
+# Replace these paths with the actual paths to your screenshots
+zoomed_out_path = '../data/far.png'
+zoomed_in_path = '../data/close.png'
+output_path = '../data/plane.png'
+
+combine_screenshots(zoomed_out_path, zoomed_in_path, output_path)
